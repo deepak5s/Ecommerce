a3e61cafe35e266774c6a9989edaa4940ecee0f0
Deepak kymar <Deepak kymar@192.168.0.3>
2017-07-13 22:52:22 +0530

    dusra

--- Indus1/src/main/java/com/project/Backend/Indus1/model/Product.java
+++ Indus1/src/main/java/com/project/Backend/Indus1/model/Product.java
@@ -1,73 +1,86 @@
 package com.project.Backend.Indus1.model;
 
-
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 @Table
-public class Product<MultipartFile> {
+public class Product {
 	@Id
 	@GeneratedValue
 	int prodid;
-int catid,suppid,quantity,price;
-String prodname;
-String proddesc;
-@Transient
-MultipartFile pimage;
+	int catid, suppid, quantity, price;
+	String prodname;
+	String proddesc;
+	@Transient
+	MultipartFile pimage;
 
+	public MultipartFile getPimage() {
+		return pimage;
+	}
 
-public MultipartFile getPimage() {
-	return pimage;
-}
-public void setPimage(MultipartFile pimage) {
-	this.pimage = pimage;
-}
-public int getProdid() {
-	return prodid;
-}
-public void setProdid(int prodid) {
-	this.prodid = prodid;
-}
-public int getCatid() {
-	return catid;
-}
-public void setCatid(int catid) {
-	this.catid = catid;
-}
-public int getSuppid() {
-	return suppid;
-}
-public void setSuppid(int suppid) {
-	this.suppid = suppid;
-}
-public int getQuantity() {
-	return quantity;
-}
-public void setQuantity(int quantity) {
-	this.quantity = quantity;
-}
-public int getPrice() {
-	return price;
-}
-public void setPrice(int price) {
-	this.price = price;
-}
-public String getProdname() {
-	return prodname;
-}
-public void setProdname(String prodname) {
-	this.prodname = prodname;
-}
-public String getProddesc() {
-	return proddesc;
-}
-public void setProddesc(String proddesc) {
-	this.proddesc = proddesc;
-}
+	public void setPimage(MultipartFile pimage) {
+		this.pimage = pimage;
+	}
+
+	public int getProdid() {
+		return prodid;
+	}
+
+	public void setProdid(int prodid) {
+		this.prodid = prodid;
+	}
+
+	public int getCatid() {
+		return catid;
+	}
+
+	public void setCatid(int catid) {
+		this.catid = catid;
+	}
+
+	public int getSuppid() {
+		return suppid;
+	}
+
+	public void setSuppid(int suppid) {
+		this.suppid = suppid;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getProdname() {
+		return prodname;
+	}
+
+	public void setProdname(String prodname) {
+		this.prodname = prodname;
+	}
+
+	public String getProddesc() {
+		return proddesc;
+	}
+
+	public void setProddesc(String proddesc) {
+		this.proddesc = proddesc;
+	}
 }
